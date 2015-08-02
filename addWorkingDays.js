18e387f59257be70cfd896971ed7b715cf532e27
Chris N <messageforchris@gmail.com>
2015-08-02 08:56:29 +0100

    Fixed links in comments

--- addWorkingDays.js
+++ addWorkingDays.js
@@ -4,7 +4,8 @@
  * A JavaScript function to add a number of working days to a date object taking account of holidays and weekends
  * Checks each day added for being a holiday or weekend, so potentially unsuitable for adding large number of days
  *
- * Holiday functionality from https://github.com/lsmith/addBusinessDays/blob/master/addBusinessDays.js
+ * Latest version at https://github.com/bluec/js-add-working-days
+ * Holiday functionality from https://github.com/lsmith/addBusinessDays
  * UK holiday dates from http://www.bluesock.com/bank_holidays_csv.php
  */
 
